e1ab11e7800bc235e6c386a6cac7dfcc70d3dcd2
William Bryan <wwbryan00@gmail.com>
2021-03-18 05:47:48 -0700

    make html routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -6,3 +6,12 @@ router.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
+// Gets to exercise page
+router.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
+});
+
+// Gets to stats page
+router.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
+});
